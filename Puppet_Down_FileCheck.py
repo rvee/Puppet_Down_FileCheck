265461b940e0470658e0e95b915910a765c2a4cc
Arvind <rvee.ash@gmail.com>
2016-12-01 01:01:03 +0530

    Reading hosts from a text file
    Hosts are now being read from a text file instead of a list

--- Puppet_Down_FileCheck.py
+++ Puppet_Down_FileCheck.py
@@ -2,14 +2,19 @@ import paramiko
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-list = ["abc-123","abc-124","abc-125"] # Random Dummy hosts for reference
+with open('Puppet_Down_Hosts.txt') as file_handler:
+        list = [str(line).rstrip('\n') for line in file_handler]
 for i in list:
-    ssh.connect(i+'.domain.com', port=22, username='admin', password ='admin')
-    stdin, stdout, stderr = ssh.exec_command('find /service/puppet -name "down"')
-    output = stdout
-    print i +'\n'.join(output)
-    if output > 5:
-        stdin, stdout, stderr = ssh.exec_command('cat /service/puppet/down')
-        down_file = stdout
-        for p in down_file: print p
+    try:
+        ssh.connect(i +'.domain.com', port=22, username='admin', password ='password')
+        stdin, stdout, stderr = ssh.exec_command('find /service/puppet -name "down"')
+        output = stdout
+        print i + '\n'.join(output)
+    except:
+        print i + " Connection Rejected"
+        continue
+if output != None:
+    stdin, stdout, stderr = ssh.exec_command('cat /service/puppet/down')
+    down_file = stdout
+    for p in down_file: print p
     ssh.close()

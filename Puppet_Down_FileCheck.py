2a0081c8fc9628e17c93137641115050eb0843ce
Arvind <rvee.ash@gmail.com>
2016-12-01 01:04:37 +0530

    Updated the comments

--- Puppet_Down_FileCheck.py
+++ Puppet_Down_FileCheck.py
@@ -2,8 +2,10 @@ import paramiko
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Reading hosts from a file
 with open('Puppet_Down_Hosts.txt') as file_handler:
         list = [str(line).rstrip('\n') for line in file_handler]
+# Connecting each host at a time and searching for a file
 for i in list:
     try:
         ssh.connect(i +'.domain.com', port=22, username='admin', password ='password')
@@ -13,6 +15,7 @@ for i in list:
     except:
         print i + " Connection Rejected"
         continue
+# printing content of a file if it exists & closing the connection
 if output != None:
     stdin, stdout, stderr = ssh.exec_command('cat /service/puppet/down')
     down_file = stdout
